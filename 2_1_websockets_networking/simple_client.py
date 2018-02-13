b14e2f2d5ab8cb13f2ccf41771fb884e8e9faed1
Charlie Friend <charles.d.friend@gmail.com>
2018-02-13 15:16:42 -0800

    2.1 Add client and server programs.

---
+++ 2_1_websockets_networking/simple_client.py
@@ -0,0 +1,32 @@
+"""
+Simple Python client program.
+Connects to a server, recieves a message and exits.
+"""
+
+import socket
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 50001
+
+
+def main():
+    # Make a socket and connect to the server
+    print("Connecting to " + SERVER_IP + ":" + str(SERVER_PORT))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER_IP, SERVER_PORT))
+
+    # Recieve a message (we'll wait to recieve 1024 bytes, or 1 kilobyte)
+    sock.settimeout(3.0)
+    server_message_binary = sock.recv(1024)
+
+    # Decode server message from binary
+    server_message = server_message_binary.decode()
+
+    # Print the message and shut down
+    print("Got message from server: " + server_message)
+    print("Shutting down...")
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2_1_websockets_networking/simple_server.py
@@ -0,0 +1,36 @@
+"""
+Simple Python server program.
+Waits for a client to connect, then sends a message.
+"""
+__author__ = "Charlie Friend"
+
+import socket
+
+MESSAGE = "Hello World! :D"
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Listen on port 50000
+    print("Waiting for connections on port 50000...")
+    sock.bind(('', 50001))
+    sock.listen()               # lets the socket listen for clients
+    conn, addr = sock.accept()  # wait for a client to connect!
+
+    # encode the message into binary
+    encoded_message = MESSAGE.encode()
+
+    # A client connected - send it a message!
+    print("Connected to " + addr[0] + "!")
+    print("Sending message: " + MESSAGE)
+    conn.sendall(encoded_message)
+
+    # Always CLOSE the socket when you're done sending messages. This tells the
+    # client that we're done talking, so it can shut down too.
+    print("Sent a message! Shutting down...")
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
