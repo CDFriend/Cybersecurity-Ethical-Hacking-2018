cc56bdf76490d9b5c4bca3e55860d97612825956
Charlie Friend <charles.d.friend@gmail.com>
2018-02-26 16:14:30 -0800

    2.2 Add encrypted client and server scripts.

---
+++ 2_2_encrypted_websockets/encrypted_client.py
@@ -0,0 +1,39 @@
+"""
+Simple Python client program.
+Connects to a server, recieves a message and exits.
+"""
+
+import pyDes
+import socket
+
+KEY = "WHATEVENISCRYPTO"
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 50000
+
+
+def main():
+    # Make a socket and connect to the server
+    print("Connecting to " + SERVER_IP + ":" + str(SERVER_PORT))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((SERVER_IP, SERVER_PORT))
+
+    # Recieve a message (we'll wait to recieve 1024 bytes, or 1 kilobyte)
+    sock.settimeout(3.0)
+    server_message_binary = sock.recv(1024)
+
+    # Create your cipher and decrypt the message
+    cipher = pyDes.triple_des(KEY, padmode=pyDes.PAD_PKCS5)
+    decrypted_message_bytes = cipher.decrypt(server_message_binary)
+
+    # Decode your newly decrypted message
+    server_message = decrypted_message_bytes.decode()
+
+    # Print the message and shut down
+    print("Got message from server: " + server_message)
+    print("Shutting down...")
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2_2_encrypted_websockets/encrypted_server.py
@@ -0,0 +1,43 @@
+"""
+Simple Python server program.
+Waits for a client to connect, then sends a message.
+"""
+__author__ = "Charlie Friend"
+
+import pyDes
+import socket
+
+KEY = "WHATEVENISCRYPTO"
+
+MESSAGE = "Shh...secret message"
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Listen on port 50000
+    print("Waiting for connections on port 50000...")
+    sock.bind(('', 50000))
+    sock.listen()               # lets the socket listen for clients
+    conn, addr = sock.accept()  # wait for a client to connect!
+
+    # encode the message into binary
+    encoded_message = MESSAGE.encode()
+
+    # create your cipher, encrypt your binary message
+    cipher = pyDes.triple_des(KEY, padmode=pyDes.PAD_PKCS5)
+    encrypted_message = cipher.encrypt(encoded_message)
+
+    # A client connected - send it a message!
+    print("Connected to " + addr[0] + "!")
+    print("Sending message: " + MESSAGE)
+    conn.sendall(encrypted_message)
+
+    # Always CLOSE the socket when you're done sending messages. This tells the
+    # client that we're done talking, so it can shut down too.
+    print("Sent a message! Shutting down...")
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
