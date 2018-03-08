ea9dd85b593fa1391c451cdc7f9327c7962b4cfd
Charlie Friend <charles.d.friend@gmail.com>
2018-03-08 12:53:25 -0800

    2.4 Add file for URL encoded parameters

---
+++ 2_4_http_intro/urlencoded_params.py
@@ -0,0 +1,29 @@
+"""
+Basic Flask web server.
+Run this program to start the web server, press CTRL+C to stop.
+"""
+__author__ = "Charlie Friend"
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    name = request.params["name"]
+    return "Hello " + name + "!"
+
+
+@app.route('/hello')
+def hello():
+    return render_template("hello.html")
+
+
+# Only run the app if this is the script being run. Otherwise importing this
+# file will start the web server.
+#
+# Using host='0.0.0.0' causes the app to listen on all interfaces, so you
+# should be able to access it from other computers on the local network.
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
