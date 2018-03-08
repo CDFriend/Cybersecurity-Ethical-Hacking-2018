734bcb4ca69a9c2dfc78a7ddb3c1d247a5a791a8
Charlie Friend <charles.d.friend@gmail.com>
2018-03-08 12:17:32 -0800

    2.4 Add HTML template example

---
+++ 2_4_http_intro/html_files.py
@@ -0,0 +1,28 @@
+"""
+Basic Flask web server.
+Run this program to start the web server, press CTRL+C to stop.
+"""
+__author__ = "Charlie Friend"
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return "Hello world!"
+
+
+@app.route('/hello')
+def hello():
+    return render_template("hello.html")
+
+
+# Only run the app if this is the script being run. Otherwise importing this
+# file will start the web server.
+#
+# Using host='0.0.0.0' causes the app to listen on all interfaces, so you
+# should be able to access it from other computers on the local network.
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
