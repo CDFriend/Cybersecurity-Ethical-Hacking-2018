bc92727133e70397c1cbceabb5b5f972a8ff18df
Charlie Friend <charles.d.friend@gmail.com>
2018-03-15 12:46:03 -0700

    3.1 Add error messages and documentation.

--- 3_1_login_pages/app.py
+++ 3_1_login_pages/app.py
@@ -1,3 +1,16 @@
+"""
+Basic login page Flask app.
+
+Note that this version of the code is not even remotely secure - please DON'T plug in
+real usernames and passwords! We'll make a secure version of this webpage in lesson
+3.2.
+
+Based on a tutorial from realpython.com:
+    https://realpython.com/blog/python/introduction-to-flask-part-2-creating-a-login-page/
+"""
+__author__ = "Charlie Friend"
+
+
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
@@ -13,6 +26,7 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    error = None
 
     # If we are POSTing to the /login route, then check the credentials!
     if request.method == "POST":
@@ -23,9 +37,10 @@ def login():
             print("Logged in!")
             return redirect("/home")
         else:
+            error = "Incorrect username or password! Please try again!"
             print("Login failed!")
 
-    return render_template('login.html')
+    return render_template('login.html', error=error)
 
 
 if __name__ == "__main__":
